289c94cf78230510d22308e347821713997790a8
50Paic <163042503+50Paic@users.noreply.github.com>
2025-07-08 16:14:21 +0200

    power_up created

---
+++ Scenes/Components/PowerUP's/powerup.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+@export var powerup_type: String = "health"  # "health", "speed", "damage"
+@export var hp_value: int = 25
+@export var xp_value: int
+@export var shield_value: int
+func _ready():
+	body_entered.connect(_on_body_entered)
+
+func _on_body_entered(body):
+	apply_powerup(body)
+	queue_free()
+
+func apply_powerup(player):
+	match powerup_type:
+		"health":
+			player.get_node("HealthShield").increase_max_hp(hp_value)
+		"shield":
+			player.get_node("HealthShield").increase_max_hp(shield_value)
+		"xp":
+			print("Player got +", xp_value, " damage!")
