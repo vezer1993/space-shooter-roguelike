65b5e51159fab2627cebc64df37c7a55402653f0
50Paic <163042503+50Paic@users.noreply.github.com>
2025-07-08 13:31:56 +0200

    bullets do dmg, enemies recive dmg

--- Scenes/Components/Shooting/Bullet type/regular_bullet.gd
+++ Scenes/Components/Shooting/Bullet type/regular_bullet.gd
@@ -4,6 +4,7 @@ var speed: float = 500.0
 var direction: Vector2 = Vector2.RIGHT
 var lifetime: float = 10.0  # Remove after 5 seconds
 var time_alive: float = 0.0
+var damage: int = 10
 
 func _ready():
 	$AnimatedSprite2D.play()  # Start the animation
@@ -22,5 +23,8 @@ func _physics_process(delta):
 	
 	# Check if bullet hit something
 	if get_slide_collision_count() > 0:
+		var collider = get_slide_collision(0).get_collider()
+		if collider.has_node("HP"):
+			collider.get_node("HP").apply_damage(damage)
 		# Hit something - destroy bullet
 		queue_free()
